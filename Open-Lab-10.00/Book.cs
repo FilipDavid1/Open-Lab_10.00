4e2d1096ef5652882d71a86626959537b5c47800
FilipDavid1 <filipenkodavid@gmail.com>
2020-11-18 16:04:58 +0100

    Call Metods

--- Open-Lab-10.00/Book.cs
+++ Open-Lab-10.00/Book.cs
@@ -6,8 +6,41 @@ namespace Open_Lab_10._00
 {
     class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public void SetTitle(string t)
+        { 
+            title = t;
+        }
+        public void SetPages(int p)
+        {
+            pages = p;
+        }
+        public void SetCategory(string c)
+        {
+            category = c;
+        }
+        public void SetAuthor(string a)
+        {
+            author = a;
+        }
+        public void SetReleaseDate(int r)
+        {
+            releaseDate = r;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("title:" + title);
+            Console.WriteLine("pages:" + pages);
+            Console.WriteLine("category:" + category);
+            Console.WriteLine("author:" + author);
+            Console.WriteLine("releaseDate:" + releaseDate);
+        }
 
     }
 }
--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -7,8 +7,14 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
            Book Lotr = new Book();
-           Lotr.title = "Polreich";
-           Console.WriteLine(Lotr.title);
+           
+           Lotr.SetTitle("Vychytávky Ladi Hrusky");
+           Lotr.SetPages(800);
+           Lotr.SetCategory("vychytavky");
+           Lotr.SetAuthor("Lada Hruska");
+           Lotr.SetReleaseDate(2018);
+
+           Lotr.Show();
 
         }
     }
